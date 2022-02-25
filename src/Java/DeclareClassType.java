42f66218b18955cf476ece000f65d1cbe81d9fe1
Nareshtester5233 <k.naresh5233@gmail.com>
2022-02-25 15:42:35 +0530

    hai

---
+++ src/Java/DeclareClassType.java
@@ -0,0 +1,26 @@
+package Java;
+
+public class DeclareClassType {
+
+	public static void main(String[] args) {
+		
+		//Declare class type
+		
+		String studentName = "Naresh";
+		String email = "naresh@gmail.com";
+		System.out.println(studentName +"  "+ email); 
+		//Declare integer
+		
+		int studentRollNumber = 8;
+		System.out.println(studentRollNumber);
+		
+		System.out.println(studentName + " " + studentRollNumber);
+		
+		System.out.println("Student Name is "+ studentName + " , "+"Student Roll Number is " + studentRollNumber + " , " + "Student Email Id is " + email );
+		
+		//Declare two integer variable
+		
+		int a=10;
+		int b=25;
+		System.out.println(a+b);
+		System.out.println("addition of a+b is " + (a+b));
